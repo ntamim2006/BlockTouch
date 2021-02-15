83a0ec50415c9b88769587cd80a653316d59bff9
nadavMac <ntamim2006@gmail.com>
2021-02-15 12:37:20 +0200

    2

--- app/src/main/java/com/nt/blocktouch/FloatingViewService.java
+++ app/src/main/java/com/nt/blocktouch/FloatingViewService.java
@@ -3,13 +3,16 @@ import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class FloatingViewService extends Service implements View.OnClickListener {
@@ -19,6 +22,11 @@ public class FloatingViewService extends Service implements View.OnClickListener
     private View mFloatingView;
     private View collapsedView;
     private View expandedView;
+    boolean over_time = false;
+    Handler handler;
+    Handler handler2;
+    Handler handler3;
+    Button b;
     private boolean ifLock = false;
     ImageView imageLock;
     WindowManager.LayoutParams params;
@@ -39,6 +47,7 @@ public class FloatingViewService extends Service implements View.OnClickListener
 
         //getting the widget layout from xml using layout inflater
         mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
+
 
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -81,42 +90,46 @@ public class FloatingViewService extends Service implements View.OnClickListener
         imageLock.setOnClickListener(this);
         mFloatingView.findViewById(R.id.exp_iv).setOnClickListener(this);
         expandedView.setOnClickListener(this);
+         b = expandedView.findViewById(R.id.button);
 
-        mFloatingView.findViewById(R.id.expClose).setOnLongClickListener(v -> {
-            //Initially view will be added to top-left corner
-            if(ifLock){
-                params = new WindowManager.LayoutParams(
-                        WindowManager.LayoutParams.WRAP_CONTENT,
-                        WindowManager.LayoutParams.WRAP_CONTENT,
-                        LAYOUT_FLAG,
-                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                        PixelFormat.TRANSLUCENT);
-                         mFloatingView.findViewById(R.id.exp_iv).setVisibility(View.VISIBLE);
-                        imageLock.setImageResource(R.drawable.lock_sign_open);
 
-            }else{
-                params = new WindowManager.LayoutParams(
-                        WindowManager.LayoutParams.MATCH_PARENT,
-                        WindowManager.LayoutParams.MATCH_PARENT,
-                        LAYOUT_FLAG,
-                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                        PixelFormat.TRANSLUCENT);
-                imageLock.setImageResource(R.drawable.lock_sign_close);
-                mFloatingView.findViewById(R.id.exp_iv).setVisibility(View.GONE);
 
-            }
-            params.gravity = Gravity.TOP | Gravity.START;        //Initially view will be added to top-left corner
-            params.x = 0;
-            params.y = 0;
-            ifLock=!ifLock;
-            mWindowManager.updateViewLayout(mFloatingView, params);
 
-            return true;
-        });
+//        mFloatingView.findViewById(R.id.expClose).setOnClickListener(v -> {
+//            //Initially view will be added to top-left corner
+//            if(ifLock){
+//                params = new WindowManager.LayoutParams(
+//                        WindowManager.LayoutParams.WRAP_CONTENT,
+//                        WindowManager.LayoutParams.WRAP_CONTENT,
+//                        LAYOUT_FLAG,
+//                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+//                        PixelFormat.TRANSLUCENT);
+//                         mFloatingView.findViewById(R.id.exp_iv).setVisibility(View.VISIBLE);
+//                        imageLock.setImageResource(R.drawable.lock_sign_open);
+//
+//            }else{
+//                params = new WindowManager.LayoutParams(
+//                        WindowManager.LayoutParams.MATCH_PARENT,
+//                        WindowManager.LayoutParams.MATCH_PARENT,
+//                        LAYOUT_FLAG,
+//                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+//                        PixelFormat.TRANSLUCENT);
+//                imageLock.setImageResource(R.drawable.lock_sign_close_2);
+//                mFloatingView.findViewById(R.id.exp_iv).setVisibility(View.INVISIBLE);
+//
+//            }
+//            params.gravity = Gravity.TOP | Gravity.START;        //Initially view will be added to top-left corner
+//            params.x = 0;
+//            params.y = 0;
+//            ifLock=!ifLock;
+//            mWindowManager.updateViewLayout(mFloatingView, params);
+//
+//
+//        });
 
 
 //        //adding an touchlistener to make drag movement of the floating widget
-//        mFloatingView.findViewById(R.id.relativeLayoutParent).setOnTouchListener(new View.OnTouchListener() {
+//        mFloatingView.findViewById(R.id.layoutExpanded).setOnTouchListener(new View.OnTouchListener() {
 //            private int initialX;
 //            private int initialY;
 //            private float initialTouchX;
@@ -147,17 +160,14 @@ public class FloatingViewService extends Service implements View.OnClickListener
 //
 //                        mWindowManager.updateViewLayout(mFloatingView, params);
 //                        return true;
-//
-//
-//
-//
-//
 //                }
 //                return false;
 //            }
 //        });
 
-
+            b.setOnClickListener(v -> {
+                Toast.makeText(this, "d", Toast.LENGTH_SHORT).show();
+            });
 
 
     }
@@ -170,16 +180,102 @@ public class FloatingViewService extends Service implements View.OnClickListener
 
     @Override
     public void onClick(View v) {
+
         switch (v.getId()) {
 
 
             case R.id.expClose:
+                if(handler!=null){
+                    handler.removeCallbacksAndMessages(null);
+                }
+                if(handler2!=null){
+                    handler2.removeCallbacksAndMessages(null);
+                }
+                if(handler3!=null){
+                    handler3.removeCallbacksAndMessages(null);
+                }
+
                 //switching views
                 if(ifLock){
-                    Toast.makeText(this, "לפתיחת נעילת המגע לחץ לחיצה ארוכה", Toast.LENGTH_SHORT).show();
+
+                        params = new WindowManager.LayoutParams(
+                                WindowManager.LayoutParams.WRAP_CONTENT,
+                                WindowManager.LayoutParams.WRAP_CONTENT,
+                                LAYOUT_FLAG,
+                                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                                PixelFormat.TRANSLUCENT);
+                        mFloatingView.findViewById(R.id.exp_iv).setVisibility(View.VISIBLE);
+                        ((ImageView)mFloatingView.findViewById(R.id.exp_iv)).setImageResource(R.drawable.close_btn_icon);
+
+                        imageLock.setImageResource(R.drawable.lock_sign_open);
+                        Toast.makeText(this, "מסך המכשיר פעיל", Toast.LENGTH_SHORT).show();
+
+                        handler = new Handler();
+                        handler.postDelayed(() -> {
+                            //write your code here to be executed after 1 second
+                            imageLock.setImageResource(R.drawable.lock_sign_open_dimmed);
+                            ((ImageView)mFloatingView.findViewById(R.id.exp_iv)).setImageResource(R.drawable.close_btn_icon_dimmed);
+                            over_time = true;
+
+
+                            }, 2000);
+                         ifLock=!ifLock;
+                         over_time = false;
+                    b.setVisibility(View.GONE);
+
+
                 }else{
-                    Toast.makeText(this, "לנעילת המגע לחץ לחיצה ארוכה", Toast.LENGTH_SHORT).show();
+//                    if(over_time){
+//                        ((ImageView)mFloatingView.findViewById(R.id.exp_iv)).setImageResource(R.drawable.close_btn_icon);
+//                        mFloatingView.findViewById(R.id.exp_iv).setVisibility(View.VISIBLE);
+//                        imageLock.setImageResource(R.drawable.lock_sign_open);
+//
+//                        over_time = false;
+//
+//                        handler2 = new Handler();
+//                        handler2.postDelayed(() -> {
+//                            //write your code here to be executed after 1 second
+//                            imageLock.setImageResource(R.drawable.lock_sign_open_dimmed);
+//                            ((ImageView)mFloatingView.findViewById(R.id.exp_iv)).setImageResource(R.drawable.close_btn_icon_dimmed);
+//
+//                            over_time = true;
+//
+//                        }, 2000);
+//                    }else {
+                        params = new WindowManager.LayoutParams(
+                                WindowManager.LayoutParams.MATCH_PARENT,
+                                WindowManager.LayoutParams.MATCH_PARENT,
+                                LAYOUT_FLAG,
+                                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                                PixelFormat.TRANSLUCENT);
+
+
+                        imageLock.setImageResource(R.drawable.lock_sign_close_2);
+                        mFloatingView.findViewById(R.id.exp_iv).setVisibility(View.INVISIBLE);
+                        Toast.makeText(this, "מסך המכשיר נעול", Toast.LENGTH_SHORT).show();
+                        b.setVisibility(View.VISIBLE);
+                       handler3 = new Handler();
+
+                        handler3.postDelayed(() -> {
+                            //write your code here to be executed after 1 second
+                            imageLock.setImageResource(R.drawable.lock_sign_close_2_dimmed);
+
+
+
+
+
+                        }, 2000);
+                        ifLock=!ifLock;
+
+//                    }
+
+
                 }
+                params.gravity = Gravity.TOP | Gravity.START;        //Initially view will be added to top-left corner
+                params.x = 0;
+                params.y = 0;
+
+                mWindowManager.updateViewLayout(mFloatingView, params);
 
                 break;
 
@@ -189,6 +285,15 @@ public class FloatingViewService extends Service implements View.OnClickListener
                 stopSelf();
 
                 break;
+
+//                case R.id.button:
+//                //closing the widget
+//                    b = v.findViewById(R.id.button);
+//                    b.setVisibility(View.GONE);
+//
+//                break;
+
+
 
         }
     }
